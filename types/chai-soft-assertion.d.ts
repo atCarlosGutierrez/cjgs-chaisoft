8b235397c150b9abf7ca7d88f167ce812b85fe87
Carlos Javier Gutierrez Sanjines <cgutierrezsanjines@gmail.com>
2025-07-06 10:41:18 -0400

    Include soft assertion collector

--- types/chai-soft-assertion.d.ts
+++ types/chai-soft-assertion.d.ts
@@ -8,9 +8,27 @@ declare global {
   }
 }
 
+interface SoftError {
+  matcher: string;
+  message: string;
+  actual?: unknown;
+  expected?: unknown;
+}
+declare class SoftAssertionCollector {
+  errors: SoftError[];
+  add(error: SoftError): void;
+  assertAll(): void;
+  clear(): void;
+}
+export declare const softAssertionsCollector: SoftAssertionCollector;
 export declare function addSoftMethod(softMethod: string | string[]): void;
 export declare function getSoftMethods(): string[];
-export declare function addSoftChainableMethod(softChainableMethod: string | string[]): void;
+export declare function addSoftChainableMethod(
+  softChainableMethod: string | string[]
+): void;
 export declare function getSoftChainableMethods(): string[];
-export declare function createSoftAssertion(newSoftMethods?: string[], newSoftChainableMethods?: string[]): Chai.ChaiPlugin;
+export declare function createSoftAssertion(
+  newSoftMethods?: string[],
+  newSoftChainableMethods?: string[]
+): Chai.ChaiPlugin;
 export declare const softAssertion: Chai.ChaiPlugin;
